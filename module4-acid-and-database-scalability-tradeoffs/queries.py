2fed64ca0ef1e2835c3f8c0cdab181c86a0c4c95
michael-s-luo <michael.luo@outlook.com>
2022-12-13 22:26:22 -0800

    finished pg queries

---
+++ module4-acid-and-database-scalability-tradeoffs/queries.py
@@ -0,0 +1,92 @@
+"""
+Unit 3.2.4 Assignment
+Date: 2022/12/13
+
+Queries to answer assignment questions for our deployed PostgreSQL database 
+(titanic data) and MongoDB (dummy RPG data).
+
+"""
+import pipeline_pg
+
+# PostgreSQL Titanic Data queries------------------------------------------------
+titanic_queries = {
+    "TOTAL_SURVIVED_VS_DIED": """
+        SELECT survived, COUNT(*) 
+        FROM titanic 
+        GROUP BY survived;
+    """,
+    "TOTAL_BY_CLASS": """
+        SELECT pclass, COUNT(*)
+        FROM titanic
+        GROUP BY pclass
+        ORDER BY pclass ASC
+    """,
+    "TOTAL_SURVIVAL_BY_CLASS": """
+        SELECT pclass, SUM(survived) as count_survived, COUNT(*) - SUM(survived) as count_died
+        FROM titanic
+        GROUP BY pclass
+        ORDER BY pclass ASC;
+    """,
+    "SURVIVED_AVG_AGE": """
+        SELECT survived, AVG(age) as avg_age
+        FROM titanic
+        GROUP BY survived;
+    """,
+    "PCLASS_AVG_AGE": """
+        SELECT pclass, AVG(age) as avg_age
+        FROM titanic
+        GROUP BY pclass
+        ORDER BY pclass ASC;
+    """,
+    "PCLASS_AVG_FARE": """
+        SELECT pclass, AVG(fare) as avg_fare
+        FROM titanic
+        GROUP BY pclass
+        ORDER BY pclass ASC;
+    """,
+    "SURVIVED_AVG_FARE": """
+        SELECT survived, AVG(fare) as avg_fare
+        FROM titanic
+        GROUP BY survived
+        ORDER BY survived DESC;
+    """,
+    "PCLASS_AVG_SIBLING/SPOUSE": """
+        SELECT pclass, AVG("siblings/spouse aboard") as avg_sibling_spouse
+        FROM titanic
+        GROUP BY pclass
+        ORDER BY pclass ASC;
+    """,
+    "SURVIVED_AVG_SIBLING/SPOUSE": """
+        SELECT survived, AVG("siblings/spouse aboard") as avg_sibling_spouse
+        FROM titanic
+        GROUP BY survived
+        ORDER BY survived DESC;
+    """,
+    "PCLASS_AVG_PARENTS/CHILDREN": """
+        SELECT pclass, AVG("parents/children aboard") as avg_parents_children
+        FROM titanic
+        GROUP BY pclass
+        ORDER BY pclass ASC;
+    """,
+    "SURVIVED_AVG_PARENTS/CHILDREN": """
+        SELECT survived, AVG("parents/children aboard") as avg_parents_children
+        FROM titanic
+        GROUP BY survived
+        ORDER BY survived DESC;
+    """,
+    # there aren't any duplicate names
+    "DUPLICATE_NAMES": """
+        SELECT t1.id, t1.name
+        FROM titanic as t1
+        INNER JOIN titanic as t2
+        ON t1.name = t2.name
+        WHERE t1.id != t2.id;
+    """,
+}
+
+
+if __name__ == "__main__":
+    pg_conn, pg_curs = pipeline_pg.con_to_pg()
+
+    for name, query in titanic_queries.items():
+        print(f"\nQuery {name} -> {pipeline_pg.query_db(pg_curs, query)}")
